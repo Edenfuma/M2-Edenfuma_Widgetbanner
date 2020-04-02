fb000ba4c5ad7e7132500d531e99681023a86206
root <eduardo.fuentes@mitocondria.cl>
2020-04-02 09:47:29 -0300

    Include app

---
+++ app/code/Edenfuma/Widgetbanner/Block/Widget/Widgetbanner.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Edenfuma\Widgetbanner\Block\Widget;
+
+use Magento\Widget\Block\BlockInterface;
+use Magento\Framework\View\Element\Template;
+
+class Widgetbanner extends Template implements BlockInterface
+{
+
+    protected $_template = "widget/widgetbanner.phtml";
+
+}
+
+
+	
